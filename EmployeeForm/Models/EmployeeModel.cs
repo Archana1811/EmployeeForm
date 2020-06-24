592caed48bab3865e368ed86b4bb1d707a0d6b02
Archana1811 <aarchu1118@gmail.com>
2020-06-24 08:58:29 +0530

    modified code

--- EmployeeForm/Models/EmployeeModel.cs
+++ EmployeeForm/Models/EmployeeModel.cs
@@ -10,19 +10,23 @@ namespace EmployeeForm.Models
         [StringLength(30, MinimumLength = 3)]
         public string Name { get; set; }
 
+        [Required(ErrorMessage = "Please Enter Age")]
         [Range(18, 25)]
         public string Age { get; set; }
 
+        [Required(ErrorMessage = "Please Enter Gender")]
         public string Gender { get; set; }
 
         [Required]
         [EmailAddress]
         public string Email { get; set; }
 
-       
+        [Required(ErrorMessage = "Please Enter City")]
         [StringLength(35)]
         public string City { get; set; }
         public string Resume { get; set; }
+
+        [Required(ErrorMessage = "Please Enter Education")]
         public string Education { get; set; }
 
 
